7dbac400a37bb29a3c9e9274e48daac8d63741db
PRAKASH RAJAN <153984812+Patlu29@users.noreply.github.com>
2025-03-20 19:12:17 +0530

    TypeORM with getRepository and manager (#1)
    * changes
    
    * TypeORM project created
    
    * table created with typeorm and data inserted
    
    * Insert, Update, Delete methods added with getRepository & manager

---
+++ TypeORM/FirstORMproject/src/data-source.ts
@@ -0,0 +1,17 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { Flight } from "./entity/Flight"
+
+export const AppDataSource = new DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "Patlu@8110",
+    database: "Flight_details",
+    synchronize: true,
+    logging: false,
+    entities: [Flight],
+    migrations: [],
+    subscribers: [],
+})
---
+++ TypeORM/FirstORMproject/src/index.ts
@@ -0,0 +1,37 @@
+import { AppDataSource } from "./data-source"
+import { Flight } from "./entity/Flight"
+
+AppDataSource.initialize().then(async () => {
+
+    const repository = AppDataSource.getRepository(Flight)
+
+// INSERT
+
+    // console.log("Inserting a new user into the database...")
+    // await repository.insert({
+    //     F_number: 953058,
+    //     F_name: "Indigo",
+    //     Destination: "Mumbai",
+    //     IsActive: false,
+    // })
+
+// UPDATE
+
+    // console.log("Updating a user into database....");
+    // await repository.update(9, {
+    //     F_name: "Boeing"
+    // })
+
+
+// DELETE 
+
+    // console.log("Deleting a flight details.....")
+    // await repository.delete(8)
+
+    console.log("Loading all flights from the database...")
+    const flights = await AppDataSource.manager.find(Flight)
+    console.log("Loaded flights: ", flights)
+
+ //   console.log("Here you can setup and run express / fastify / any other framework.")
+
+}).catch(error => console.log(error))
